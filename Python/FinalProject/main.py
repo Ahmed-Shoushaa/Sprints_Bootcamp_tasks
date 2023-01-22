d2b545ef708fe1652b98d179251f2874c14c6199
ahmed <ahmed.adel1199@yahoo.com>
2023-01-22 08:48:14 +0200

    final code commit isa

--- Python/FinalProject/main.py
+++ Python/FinalProject/main.py
@@ -1,23 +1,55 @@
 import csv
 from datetime import date
 import os
-from botocore.exceptions import ClientError
-import boto3
 
 today = date.today()
 filename = "ContactBook_" + str(today) + ".csv"
-header = ["username", "email", "phone numbers", "address", "insertion date"]
+header = ["UserName", "Email", "Phone Numbers", "Address", "Insertion Date"]
 
+#Make sure the user input is in the right format
+def input_in_valid_format():
+    while True:
+        x = input()
+        added_line = x.split(",")
+        if len(added_line) == 4 and added_line[1].__contains__("@") and added_line[2].isdigit() == True:
+            break
+        else:
+            print("Your Input is in the wrong format please re enter 4 elemnts seperated with a comme ',' with no spaces "
+                  "\n1-UserName(can contain any character)"
+                  "\n2-Email(must contain @ symbol)"
+                  "\n3-Phone Number(must contain numbers only)"
+                  "\n4-Address(can contain any character)\n.........\nTry Again\n.....")
+    added_line.append(str(today))
+    return added_line
+
+def todays_file_exist():
+    # Specify file
+    file = filename
+    #Return true or false depends on file existance
+    isExist = os.path.exists(file)
+    if isExist == True:
+        print(file)
+        print("==========================")
+        pass
+    else:
+        print("There is no Contact Book with today's date")
+        with open(filename, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+        print(file,"is Created")
+#show a list of users in the csv file (first column)
 def list_of_users_in_csvFile(file):  #Function to list all the users in file
     names = []
     with open(file, 'r') as ff:
+
         read = csv.reader(ff)
+        next(read)
         for row in read:
             names.append(row[0])
     return names
 
+#Welcome function to let the user decide what command he wants to do with the app
 def welcome():
-# This is a welcome function to let the user decide what he wants to do in the app
     print("WELCOME BACK TO YOUR CONTACT BOOK\n=================================")
     print("What do you need to do?\n.......................")
     print("1-Create New Contact\n2-Update Old Contact\n3-Delete a Contact\n4-Backup Contact Book\n5-Show Contact Book")
@@ -25,8 +57,8 @@ def welcome():
     print("=====================")
     return cmd
 
+#Depending on user command from welcome function the program flow
 def cases(cmd_num):
-# Take welcome function output and feed it to the cases function to decide the program flow
     if cmd_num == "1":
         print("CONTACTS CREATION\n~~~~~~~~~~~~~~~~~")
         create()
@@ -39,10 +71,11 @@ def cases(cmd_num):
     elif cmd_num == "4":
         print("BACKUP CONTACT BOOK\n~~~~~~~~~~~~~~~~~~~")
         backup(filename,"ahmedadelelsayed")
+
     elif cmd_num == "5":
         show()
     else:
-        print("Please enter number between 1 and 5")
+        print("You Enterd Wrong Command Number")
 
 def create():
     print("How do you want to add data?\n...........................\n1-Add manually\n2-Add from file")
@@ -51,26 +84,37 @@ def create():
     if a == "1":
         print(".................\nMANUAL DATA ENTRY\n.................")
         print("ENTER YOUR CONTACT IN CSV FORMAT \nUserName,Email,Number,Address(the insertion date will be added automatically)")
-        #Take User Input
-        x = input() + ',' + str(today)    # x will be at csv format
-        added_line = x.split(",")         # turn x elements into a list to add easily to the file
+        #Take User Input and make sure it's in the right format
+        added_line = input_in_valid_format()
         # open file in append mode to add the new line
         with open(filename, 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(added_line)
-        print('CONTACT:'+ x ,'ADDED SUCCESSFULLY')
+        print('CONTACT:', added_line, 'ADDED SUCCESSFULLY')
     #Condition 2 for data entry from a file
     elif a == "2":
         print(".....................\nIMPORT DATA FROM FILE\n.....................")
         file = input("Enter File Name with it's extension:")
-        #open the imported file in read mode and the original file in write mode then loop over imported file rows and add to the original one
-        with open(file, mode="r") as old_file:
-            reader = csv.reader(old_file)
+        try:
+            # open the imported file in read mode and the original file in write mode then loop over imported file rows and add to the original one
+            data =[]
+            with open(file, mode="r",newline='') as old_file:
+                reader = csv.reader(old_file)
+                for row in reader:
+                    data.append(row)
             with open(filename, mode="a", newline='') as new_file:
                 writer = csv.writer(new_file)
-                for data in reader:
-                    writer.writerow(data)
-        print(file,"IMPORTED SUCCESSFULLY")
+                for row in data:
+                    writer.writerow(row)
+                    # If condition to pass input file lines in format and remove others
+                    # for data in reader:
+                    #     if len(data) == 4 and data[1].__contains__("@") and data[2].isdigit() == True:
+                    #         writer.writerow(data)
+                    # else:
+                    #     pass
+            print(file,"IMPORTED SUCCESSFULLY")
+        except:
+            print(file,"DOESN'T EXISTS")
     else:
         print("You entered wrong command number")
     return filename
@@ -81,11 +125,16 @@ def update():
     #This function prints user names in the csv file
     users_list = list_of_users_in_csvFile(filename)
     print("CONTACTS:",users_list)
-    #take input from user name and updated data from user
-    username = input("Enter the user you want to update his data from user list above:")
+    #This block to make sure the user entered a valid input
+    while True:
+        username = input("Enter the user you want to update his data from Contacts above:")
+        if username in users_list:
+            break
+        else:
+            print(username,"DOESN'T EXISTS ENTER A valid ONE")
     print("Enter your updated data in CSV format\nUserName,Email,Number,Address,(the insertion date will be added automatically)")
-    x = input() + ',' + str(today)  #updated contact in csv format
-    updated_row = x.split(",")      #turn it into a list to add to file easily
+    #x = input() + ',' + str(today)  #updated contact in csv format
+    updated_row = input_in_valid_format()      #turn it into a list to add to file easily
     #For function to get the upsated row index as i
     for i in range (len(users_list)):
         if username == users_list[i]:
@@ -108,12 +157,19 @@ def update():
 def delete():
     updatedlist = []
     #This function prints user names in the csv file
-    print("CONTACTS:",list_of_users_in_csvFile(filename))
+    users_list = list_of_users_in_csvFile(filename)
+    print("CONTACTS:", users_list)
     #Main logic for row deleting
     ##Open the file we want to delete from
     with open(filename, newline="") as f:
         reader = csv.reader(f)
-        username = input("Enter the username from the list above to remove from file:")
+        #This block to make sure the user entered a valid input
+        while True:
+            username = input("Enter the username you want to delete his data from Contacts:")
+            if username in users_list:
+                break
+            else:
+                print(username, "DOESN'T EXISTS ENTER A valid ONE")
         ##check for the wanted row and adding other rows to a new file
         for row in reader:
             if username not in row:
@@ -122,27 +178,39 @@ def delete():
         with open(filename, "w", newline="") as f:
             Writer = csv.writer(f)
             Writer.writerows(updatedlist)
-            print("File has been updated")
+            print(username,"Deleted successfully and File has been updated")
 
 def backup(file_name, bucket, object_name=None, folder_name=None):
+    from botocore.exceptions import ClientError
+    import boto3
     # Upload the file
     try:
         s3_client = boto3.client(service_name='s3', aws_access_key_id="AKIA2J67UWKQ6TGKOK6V",
                                  aws_secret_access_key="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")
         # Upload the file to the s3
         response = s3_client.upload_file(file_name, bucket, file_name)
-        print("Backup succeeded")
+        print(file_name,"Backed up successfully on S3Bucket:",bucket)
     except ClientError as e:
         print(e)
 
 def show():
-    with open(filename, mode="r") as csv_file:
+    print("SHOWING CONTACTS BOOK\n~~~~~~~~~~~~~~~~~~~~~")
+    print(filename,"\n~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    with open(filename, "r") as csv_file:
         reader = csv.reader(csv_file)
         for item in reader:
             print(','.join(item))
+    print("")
 
 
-tt= input('ggg')
-cmd_num = welcome()
-cases(cmd_num)
-input('Press ENTER to exit')
+
+todays_file_exist()
+
+while True:
+    cmd_num = welcome()
+    cases(cmd_num)
+    x= input("DO YOU WANT Another Service?(y,n)")
+    if x =="y":
+        pass
+    else:
+        break
